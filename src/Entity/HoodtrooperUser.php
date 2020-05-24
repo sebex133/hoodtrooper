0a439c81ca252e0da5324f6cf6c5546968ea9bea
sebex133 <sebex133@wp.pl>
2020-05-24 15:01:01 +0200

    HoodtroperUser register form

--- src/Entity/HoodtrooperUser.php
+++ src/Entity/HoodtrooperUser.php
@@ -4,10 +4,13 @@ namespace App\Entity;
 
 use App\Repository\HoodtrooperUserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=HoodtrooperUserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"nickname"}, message="There is already an account with this nickname")
  */
 class HoodtrooperUser implements UserInterface
 {
